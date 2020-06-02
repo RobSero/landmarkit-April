d44fcd986e1de9fc7ed7be1c32132362ea0ea39c
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-06-02 08:48:39 +0100

    added CRUD controllers

--- RobSero/django-api/buildings/views.py
+++ RobSero/django-api/buildings/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK
+from rest_framework import status
 from .models import Building
 from .serializers import BuildingSerializer
+from rest_framework.exceptions import NotFound
 
 # Create your views here.
 class BuildingIndex(APIView):
     def get(self, req):
         buildings = Building.objects.all()
         serialized_buildings = BuildingSerializer(buildings, many=True)
-        return Response(serialized_buildings.data, status=HTTP_200_OK)
+        return Response(serialized_buildings.data, status=status.HTTP_200_OK)
     
 
 class BuildingShow(APIView):
-    def get(self, req, pk):
+    
+    def get_building(self,pk):
         try:
-            building = Building.objects.filter(pk=pk)
-            serialized_building = BuildingSerializer(building, many=True)
-            return Response(serialized_building.data, status=HTTP_200_OK)
+            return Building.objects.get(pk=pk)
         except Building.DoesNotExist:
-            return Response({'message' : 'Building Not Found :('}, status=HTTP_404_NOT_FOUND)
+            raise NotFound()
+    
+    
+    def get(self, req, pk):
+            building = self.get_building(pk)
+            serialized_building = BuildingSerializer(building)
+            return Response(serialized_building.data, status=status.HTTP_200_OK)
         
+    
+    def put(self,req, pk):
+        building_to_update = self.get_building(pk)
+        updated_building = BuildingSerializer(building_to_update, data=req.data)
+        if updated_building.is_valid():
+            updated_building.save()
+            return Response(updated_building.data, status=status.HTTP_201_CREATED) 
+    
+    
+    def delete(self,req, pk):
+        building_to_delete = self.get_building(pk)
+        building_to_delete.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
